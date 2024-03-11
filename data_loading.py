1f25e9bc51615d3bf9f0f8d78c3e28f1c7636a99
Omkar Pote <omkrpt@gmail.com>
2024-03-11 20:19:17 +0530

    remove other code

--- data_loading.py
+++ data_loading.py
@@ -29,33 +29,13 @@ from tqdm import tqdm
 
 
 def get_dataset(set_name, binary=False, window_size = 10, exclude_columns=[], **kwargs):
-    if set_name in ['cstr', 'pronto', 'cstr2']:
-        kwargs['window_size'] = window_size
-        kwargs['windowize'] = False
-        kwargs['skip'] = 1
-        kwargs['trim'] = 1
-        # kwargs['noise_scale'] = 1
-        # kwargs['test_split_size'] = 1
-        return get_dataset_csv(set_name, exclude_columns, **kwargs)
-    if set_name not in ['taxonomist', 'hpas', 'test', 'natops']:
-        raise ValueError("Wrong set_name")
-    if binary:
-        if set_name in ['taxonomist', 'test', 'natops']:
-            kwargs['make_binary'] = True
-        elif set_name == 'hpas':
-            kwargs['classes'] = ['none', 'dcopy']
-    rootdir = Path(kwargs.get('rootdir', './data'))
-    if set_name == 'taxonomist':
-        kwargs['window'] = 45
-        kwargs['skip'] = 45
-    if set_name == 'test':
-        set_name = 'taxonomist'
-        kwargs['window'] = 60
-        kwargs['skip'] = 60
-        kwargs['test'] = True
-    if set_name == 'natops':
-        kwargs['windowize'] = False
-    return load_hpc_data(rootdir / set_name, **kwargs)
+    kwargs['window_size'] = window_size
+    kwargs['windowize'] = False
+    kwargs['skip'] = 1
+    kwargs['trim'] = 1
+    # kwargs['noise_scale'] = 1
+    # kwargs['test_split_size'] = 1
+    return get_dataset_csv(set_name, exclude_columns, **kwargs)
 
 def drop_columns(timeseries):
     return timeseries.drop(
@@ -77,71 +57,6 @@ def process_data(timeseries, labels, classes=None, **kwargs):
     timeseries = timeseries.dropna(axis=0)
     assert(not timeseries.isnull().any().any())
     return timeseries, labels
-
-
-def load_hpc_data(data_folder, make_binary=False, for_autoencoder=False, **kwargs):
-    if for_autoencoder:
-        # Only get data from a single hardware node
-        if 'none' not in kwargs.get('classes'):
-            raise ValueError("Autoencoder has to train with healthy class")
-        nodeid_df = pd.read_csv(data_folder / 'nids.csv')
-        labels = pd.concat([pd.read_hdf(data_folder / 'train_labels.hdf'),
-                            pd.read_hdf(data_folder / 'test_labels.hdf')])
-        labels = labels[labels['label'].isin(kwargs.get('classes'))]
-        best_nid = 0
-        best_count = 0
-        for nid in nodeid_df['nid'].unique():
-            node_ids = nodeid_df[nodeid_df['nid'] == nid]['node_id']
-            if len(labels.loc[node_ids, :, :]['label'].unique()) == 1:
-                continue
-            min_count = labels.loc[node_ids, :, :]['label'].value_counts().min()
-            if min_count > best_count:
-                best_nid = nid
-                best_count = min_count
-
-        node_ids = nodeid_df[nodeid_df['nid'] == best_nid]['node_id']
-        labels = labels.loc[node_ids, :, :]
-        logging.info("Returning runs from nid000%d, counts: %s",
-                     best_nid, labels['label'].value_counts().to_dict())
-        timeseries = pd.concat([pd.read_hdf(data_folder / 'train.hdf'),
-                                pd.read_hdf(data_folder / 'test.hdf')])
-
-        train_nodeids, test_nodeids = train_test_split(
-            labels.index.get_level_values('node_id').unique(), test_size=0.2, random_state=0)
-        test_timeseries = timeseries.loc[test_nodeids, :, :]
-        test_labels = labels.loc[test_nodeids, :, :]
-        timeseries = timeseries.loc[train_nodeids, :, :]
-        labels = labels.loc[train_nodeids, :, :]
-    else:
-
-        timeseries = pd.read_hdf(data_folder / 'train.hdf')
-        labels = pd.read_hdf(data_folder / 'train_labels.hdf')
-        labels['label'] = labels['label'].astype(str)
-        if make_binary:
-            label_to_keep = labels.mode()['label'][0]
-            labels[labels['label'] != label_to_keep] = 'other'
-
-        test_timeseries = pd.read_hdf(data_folder / 'test.hdf')
-        test_labels = pd.read_hdf(data_folder / 'test_labels.hdf')
-        test_labels['label'] = test_labels['label'].astype(str)
-        if make_binary:
-            test_labels[test_labels['label'] != label_to_keep] = 'other'
-
-    timeseries, labels = process_data(timeseries, labels, **kwargs)
-    assert(not timeseries.isnull().any().any())
-    test_timeseries, test_labels = process_data(
-        test_timeseries, test_labels, **kwargs)
-    assert(not test_timeseries.isnull().any().any())
-
-    # Divide test data
-    if kwargs.get('test', False):
-        test_node_ids = [
-            test_labels[test_labels['label'] == label].index.get_level_values('node_id')[0]
-            for label in test_labels['label'].unique()]
-        test_labels = test_labels.loc[test_node_ids, :]
-        test_timeseries = test_timeseries.loc[test_node_ids, :]
-
-    return timeseries, labels, test_timeseries, test_labels
 
 def get_dataset_csv(set_name, exclude_columns, window_size, **kwargs):
     pattern = kwargs.get('pattern', "*.csv")
